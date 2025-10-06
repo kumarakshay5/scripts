d5156d81a0b1e5b21ec9a455cea068a879ac35cc
Akshay Kumar <125215478+kumarakshay5@users.noreply.github.com>
2025-10-06 15:28:10 +0530

    Update Kubectl_install.sh

--- Kubectl_install.sh
+++ Kubectl_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.3/2025-08-03/bin/linux/amd64/kubectl.sha256
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.3/2025-08-03/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
